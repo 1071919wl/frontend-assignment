f4b1dd5e3e7cf39a13028d921fef47052219d967
William Leung <w.leung.93@gmail.com>
2021-06-13 10:23:37 -0700

    complete modal info

--- src/components/movieList/movieDetail.jsx
+++ src/components/movieList/movieDetail.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import './stylesheets/movieDetail.css';
 
 const MovieDetail = ({movie}) => {
+    console.log(movie)
+
+    const formatDate = (releaseDate) => {
+        let date = new Date(releaseDate)
+        let readable = date.toDateString();
+        let arr = readable.split(" ");
+        arr.shift();
+        return arr.join(" ")
+    }
 
     return (
         <div>
-            <h1>LOOOOKIE!</h1>
-            <div>
+            <div className='detailTitleContainer'>
                 <h1>{movie.title}</h1>
             </div>
-            <div>
+            <div className='imgDetailContainer'>
                 <div>
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt='img' className='movieImg'/>
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt='img' className='detailImg'/>
                 </div>
-                <div>
-
+                <div className='dateVoteContainer'>
+                    <div className='releaseDateContainer'>
+                        <label className='dateLabel'>Release Date:</label>
+                        {/* <p>{movie.release_date}</p> */}
+                        <p>{formatDate(movie.release_date)}</p>
+                    </div>
+                    <div className='overviewContainer'>
+                        <p>{movie.overview}</p>
+                    </div >
+                    <div className='voteAvgContainer'>
+                        <p className='voteAvg'>{movie.vote_average}</p> 
+                        <p>/ 10 ({movie.vote_count} total votes)</p>
+                    </div>
                 </div>
             </div>
         </div>
